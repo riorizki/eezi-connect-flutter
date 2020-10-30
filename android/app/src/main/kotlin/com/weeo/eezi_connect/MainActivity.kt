0158e49cddee84ba2dd7f5865d514f2027ecc2d0
Rio Rizki <rio.rizkiw21@gmail.com>
2020-10-31 00:16:23 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/weeo/eezi_connect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.weeo.eezi_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/ColorConfig.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const COLOR_PRIMARY_1 = Color(0xFF022C43);
+const COLOR_PRIMARY_2 = Color(0xFF115173);
+const COLOR_ACCENT = Color(0xFFFFD700);
+const COLOR_WHITE = Color(0xFFFFFFFF);
+const COLOR_GRAY = Color(0xFFCCCCCC);
+const COLOR_DARK_GRAY = Color(0xFF999999);
+const COLOR_RED = Color(0xFFF34545);
+const COLOR_PURPLE = Color(0xFF9874FF);
+const COLOR_CONNECT_1 = Color(0xFF4B5E5B);
